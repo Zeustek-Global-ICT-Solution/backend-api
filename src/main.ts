835531d3ed98e456fbb81b54525f0994b95fa0c6
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-01-14 00:45:46 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
 import { ResponseFilter } from '@app/shared';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
