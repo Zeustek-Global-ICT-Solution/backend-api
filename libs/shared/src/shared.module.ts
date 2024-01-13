ed1dc1217deb1d3856ddc70ac31db1a59a4d5909
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-01-13 16:44:37 +0100

    added databse connection

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SharedService } from './shared.service';
 import { CoreModule } from './core/core.module';
 import { DatabaseModule } from './database/database.module';
+import { OpenaiModule } from './openai/openai.module';
 
 @Module({
   providers: [SharedService],
   exports: [SharedService],
-  imports: [CoreModule, DatabaseModule],
+  imports: [CoreModule, DatabaseModule, OpenaiModule],
 })
 export class SharedModule {}
