e4b7fa6c5717f3f3b435c2e835b19097ec3702b2
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-01-15 23:46:17 +0100

    update cache

--- libs/shared/src/core/core.module.ts
+++ libs/shared/src/core/core.module.ts
@@ -1,36 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Module } from '@nestjs/common';
-import { redisStore } from 'cache-manager-redis-store';
-import { CacheModule, CacheStore } from '@nestjs/cache-manager';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-yet';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    CacheModule.registerAsync({
-      isGlobal: true,
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        ttl: 600,
-        auth_pass: 'AbUcLDwFV3YQQtTrUzdAcGpePxfhPiOUcAzCaMZV3nk=',
+    CacheModule.register({
+      store: redisStore,
+      port: 6379,
+      auth_pass: 'AbUcLDwFV3YQQtTrUzdAcGpePxfhPiOUcAzCaMZV3nk=',
+      host: 'jummaiCacheStore.redis.cache.windows.net',
+      tls: {
         host: 'jummaiCacheStore.redis.cache.windows.net',
-        port: 6380,
-        tls: {
-          host: 'jummaiCacheStore.redis.cache.windows.net',
-        },
-        store: (await redisStore({})) as unknown as CacheStore,
-      }),
-      inject: [ConfigService],
+      },
+      isGlobal: true,
     }),
-    // CacheModule.register({
-    //   store: redisStore,
-    //   port: 6380,
-    //   auth_pass: 'AbUcLDwFV3YQQtTrUzdAcGpePxfhPiOUcAzCaMZV3nk=',
-    //   host: 'jummaiCacheStore.redis.cache.windows.net',
-    //   tls: {
-    //     host: 'jummaiCacheStore.redis.cache.windows.net',
-    //   },
-    //   isGlobal: true,
-    // }),
   ],
 })
 export class CoreModule {}
