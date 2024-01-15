4a0846f11a0d9feef7de3dd799632edda7be7007
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-01-16 00:05:37 +0100

    update cache

--- libs/shared/src/core/core.module.ts
+++ libs/shared/src/core/core.module.ts
@@ -6,7 +6,7 @@ import { CacheModule } from '@nestjs/cache-manager';
   imports: [
     CacheModule.register({
       store: redisStore,
-      port: 6379,
+      port: 6380,
       auth_pass: 'AbUcLDwFV3YQQtTrUzdAcGpePxfhPiOUcAzCaMZV3nk=',
       host: 'jummaiCacheStore.redis.cache.windows.net',
       tls: {
